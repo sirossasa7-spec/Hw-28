53d2cd762b96d763d701bd4475d80779bd23fd5b
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-05-16 21:43:15 +0200

    Truck.cpp

--- Hw-28/Truck.cpp
+++ Hw-28/Truck.cpp
@@ -1,1 +1,19 @@
 #include "Truck.h"
+
+Truck::Truck(string b, string c, double tank, double consumption, double capacity)
+    : PassengerCar(b, c, tank, consumption) {
+    loadCapacity = capacity;
+}
+
+Truck::~Truck() {
+    cout << "Об'єкт Truck знищено" << endl;
+}
+
+double Truck::costPerTonKm(double fuelPrice) const {
+    return ((fuelConsumption / 100) * fuelPrice) / loadCapacity;
+}
+
+void Truck::showTruckData() const {
+    showData();
+    cout << "Вантажопідйомність: " << loadCapacity << " т" << endl;
+}

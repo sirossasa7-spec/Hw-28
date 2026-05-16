235dc50be4488760f4d82c97d3aab372c7d68b11
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-05-16 21:36:50 +0200

    Car.h

--- Hw-28/Car.h
+++ Hw-28/Car.h
@@ -1,5 +1,19 @@
-#pragma once
-class Car
-{
-};
+#include <iostream>
+#include <string>
+using namespace std;
 
+
+class PassengerCar {
+protected:
+    string brand;
+    string color;
+    double fuelTankVolume;
+    double fuelConsumption;
+
+public:
+    PassengerCar(string b, string c, double tank, double consumption);
+
+    void setData(string b, string c, double tank, double consumption);
+    void showData() const;
+    double calculateDistance() const;
+};

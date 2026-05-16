c86e2b44a25b7ce5207a8dc60709932950860ee8
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-05-16 21:39:51 +0200

    Car.cpp

--- Hw-28/Car.cpp
+++ Hw-28/Car.cpp
@@ -1,2 +1,26 @@
 #include "Car.h"
 
+PassengerCar::PassengerCar(string b, string c, double tank, double consumption) {
+    brand = b;
+    color = c;
+    fuelTankVolume = tank;
+    fuelConsumption = consumption;
+}
+
+void PassengerCar::setData(string b, string c, double tank, double consumption) {
+    brand = b;
+    color = c;
+    fuelTankVolume = tank;
+    fuelConsumption = consumption;
+}
+
+void PassengerCar::showData() const {
+    cout << "Марка: " << brand << endl;
+    cout << "Колір: " << color << endl;
+    cout << "Об'єм бака: " << fuelTankVolume << " л" << endl;
+    cout << "Витрата: " << fuelConsumption << " л/100км" << endl;
+}
+
+double PassengerCar::calculateDistance() const {
+    return (fuelTankVolume / fuelConsumption) * 100;
+}
